7aca5a690da9fc4e94372b3ab70e8944f8aaacdc
moms2700 <85704249+moms2700@users.noreply.github.com>
2026-05-17 17:20:38 +0200

    Implement NLP preprocessing (NLP 2/4)

---
+++ data/nlp_preprocessing.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+# torch.tensor(python_list) returns a Python list as a tensor
+class Solution:
+    def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
+        words = set()
+        combined = positive + negative
+        for sentence in combined:
+            for word in sentence.split():
+                words.add(word)
+        sorted_list = sorted(list(words))
+        word_to_int = {}
+        for i, c in enumerate(sorted_list):
+            word_to_int[c] = i+1
+
+        def encode(sentence):
+            integers = []
+            for word in sentence.split():
+                integers.append(word_to_int[word])
+            return integers
+
+        var_len_tensors = []
+        for sentence in combined:
+            var_len_tensors.append(torch.tensor(encode(sentence)))
+        return nn.utils.rnn.pad_sequence(var_len_tensors, batch_first = True)
+
+            
